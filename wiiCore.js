e858068b1fe24b44842505e1920d442d8eedaad2
CornierKhan1 <jordyalexlyall@gmail.com>
2020-05-22 22:55:47 +1000

    rev9: music and iframe.html

--- wiiCore.js
+++ wiiCore.js
@@ -66,11 +66,37 @@ const shop = new wiiShop();
 // ECommerceInterface object.
 const ec = new ECommerceInterface();
 
+var sound = new wiiSound()
+
+/*
+sound.playBGM() - play the background music
+sound.playSE(id) - play a sound
+=====sound ids:=====
+1: nothing
+2: focus
+3: select (pressed)
+4: cancel
+5: keyboard - switch
+6: keyboard - empty
+7: dl success?
+8: update success?
+9: mario: jump
+10: mario: jump (low)
+11: mario: fireball
+12: mario: coin
+13: mario: brick block?
+14: mario: squash
+15: loading 1
+16: loading 2
+apparently you can stop both loading sounds using the success sounds
+*/
+
 // Mitigates timeouts.
 const connected = shop.connecting;
 
 // initialiseShop: Mitigates the timeout error.
 function initialiseShop() {
+    sound.playBGM();
     ec.setWebSvcUrls(ecsUrl, iasUrl, casUrl);
     ec.setContentUrls(ccsUrl, ucsUrl);
 }
