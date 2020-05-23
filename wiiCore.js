01b6177bcb12ca77cdde1671ddf33aa6190d3519
CornierKhan1 <jordyalexlyall@gmail.com>
2020-05-23 14:29:34 +1000

    Add warning and assign new service urls.

--- wiiCore.js
+++ wiiCore.js
@@ -1,30 +1,58 @@
-// wiiCore.js: minified for essential functions. Expands when we are ready to test a new function on WSC.
-
-const ecTimeout                 = null;
-const useSyncRegistration       = null;
-const ecsUrl = "https://ecs.s.oscwii.org";
-const iasUrl = "https://ecs.s.oscwii.org";
-const ccsUrl = "https://ecs.s.oscwii.org";
-const ucsUrl = "https://ecs.s.oscwii.org";
-const casUrl = "https://ecs.s.oscwii.org";
-
-var progress;
-
-
-
-// Keyboard object.
-const keyboard = new wiiKeyboard();
 /*
-keyboard.call(type, limitrow, secret) - call a keyboard!
-	limitrow - amount of rows allowed?
-	secret - replaces with dots? (Boolean)
-=====keyboard types:=====
-0: default
+
+    [!] IMPORTANT [!]
+    Do NOT use let. It is not supported by Wii Shop Channel.
+    Use var instead. Pull requests using let may not be accepted.
+
+    wiiCore.js: JS Library for controlling the Wii Shop Channel.
+
 */
 
-// WiiShop object.
+// Nullify some unimportant variables.
+const ecTimeout                 = null;
+const useSyncRegistration       = null;
+
+// Service URLs.
+const ecsUrl = "https://ecs.s.oscwii.org";
+const iasUrl = "https://ias.s.oscwii.org";
+const ccsUrl = "https://ccs.s.oscwii.org";
+const ucsUrl = "https://ucs.s.oscwii.org";
+const casUrl = "https://cas.s.oscwii.org";
+var progress;
+
+// Documentation at the bottom of the file.
+const keyboard = new wiiKeyboard();
 const shop = new wiiShop();
+const ec = new ECommerceInterface();
+const sound = new wiiSound()
+const connected = shop.connecting;
+
+
+// initialiseShop(): Prepares the services for use.
+function initialiseShop() {
+    sound.playBGM();
+    ec.setWebSvcUrls(ecsUrl, iasUrl, casUrl);
+    ec.setContentUrls(ccsUrl, ucsUrl);
+}
+
+// getECLog(): Returns the ECommerce Library log. (Good for Eval page.)
+function getECLog() {
+    document.getElementById('evalOutput').value = ec.getLog();
+}
+
+// dl(titleId): Downloads a title from WSC. TitleId must be a string.
+function dl(titleId) {
+    progress = ec.downloadTitle(titleId);
+    document.getElementById('evalOutput').value = progress;
+}
+
 /*
+    keyboard.call(type, limitrow, secret) - call a keyboard!
+        limitrow - amount of rows allowed?
+        secret - replaces with dots? (Boolean)
+    =====keyboard types:=====
+    0: default
+
   shop.returnToUpdate() - Go to Wii System Update in the settings
   shop.rebootSystem() - Reboots the system? (Freezes Dolphin)
   shop.returnToMenu() - Returns to the Wii Menu
@@ -61,53 +89,25 @@ const shop = new wiiShop();
     0 - Internet
     1 - Server
     2 - Online
-*/
 
-// ECommerceInterface object.
-const ec = new ECommerceInterface();
-
-var sound = new wiiSound()
-
-/*
-sound.playBGM() - play the background music
-sound.playSE(id) - play a sound
-=====sound ids:=====
-1: nothing
-2: focus
-3: select (pressed)
-4: cancel
-5: keyboard - switch
-6: keyboard - empty
-7: dl success?
-8: update success?
-9: mario: jump
-10: mario: jump (low)
-11: mario: fireball
-12: mario: coin
-13: mario: brick block?
-14: mario: squash
-15: loading 1
-16: loading 2
-apparently you can stop both loading sounds using the success sounds
-*/
-
-// Mitigates timeouts.
-const connected = shop.connecting;
-
-// initialiseShop: Mitigates the timeout error.
-function initialiseShop() {
-    sound.playBGM();
-    ec.setWebSvcUrls(ecsUrl, iasUrl, casUrl);
-    ec.setContentUrls(ccsUrl, ucsUrl);
-}
-
-// getECLog: Returns the ECommerce Library log. (Good for Eval page.)
-function getECLog() {
-    document.getElementById('evalOutput').value = ec.getLog();
-}
-
-// dl(titleId): Downloads a title from WSC. TitleId must be a string.
-function dl(titleId) {
-    progress = ec.downloadTitle(titleId);
-    document.getElementById('evalOutput').value = progress;
-}
+    sound.playBGM() - play the background music
+    sound.playSE(id) - play a sound
+    =====sound ids:=====
+    1: nothing
+    2: focus
+    3: select (pressed)
+    4: cancel
+    5: keyboard - switch
+    6: keyboard - empty
+    7: dl success?
+    8: update success?
+    9: mario: jump
+    10: mario: jump (low)
+    11: mario: fireball
+    12: mario: coin
+    13: mario: brick block?
+    14: mario: squash
+    15: loading 1
+    16: loading 2
+    apparently you can stop both loading sounds using the success sounds
+ */
